a530d2a2b7a26da10d0e258210294c61ff304f2f
Mahyar <137426475+mahyarkhn@users.noreply.github.com>
2024-02-02 17:05:34 +0330

    add posts automation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+loremimpsum = "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+
 $(document).ready(function() {
-    console.log("Hello World")
-})
+    for(i = 0; i < 10; i++){
+        $(".main_container").append(`
+            <section class="main_post">
+                <div class="main_post_data">
+                    <h2 class="main_post_title">Post ${i+1}</h2>
+                    <p class="main_post_desc">${loremimpsum}</p>
+                    <a class="main_post_link" href=".">Read more...</a>
+                </div>
+                <a class="main_post_pic" href=".">
+                    <img alt="post preview" src="https://picsum.photos/200?random=${i}"/>
+                </a>
+            </section>`
+        )}
+    }
+
+);
+
+function base64Encode(str) {
+    var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var out = "", i = 0, len = str.length, c1, c2, c3;
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += CHARS.charAt(c1 >> 2);
+            out += CHARS.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += CHARS.charAt(c1 >> 2);
+            out += CHARS.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+            out += CHARS.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += CHARS.charAt(c1 >> 2);
+        out += CHARS.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += CHARS.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += CHARS.charAt(c3 & 0x3F);
+    }
+    return out;
+}
