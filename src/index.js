e6064df496ea78e2b72dc06947ddd4b5604a9812
Mahyar <137426475+mahyarkhn@users.noreply.github.com>
2024-02-03 23:44:07 +0330

    Updated styling

--- src/index.js
+++ src/index.js
@@ -1,47 +1,32 @@
 loremimpsum = "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+let i = 0
 
 $(document).ready(function() {
-    for(i = 0; i < 10; i++){
-        $(".main_container").append(`
-            <section class="main_post">
-                <div class="main_post_data">
-                    <h2 class="main_post_title">Post ${i+1}</h2>
-                    <p class="main_post_desc">${loremimpsum}</p>
-                    <a class="main_post_link" href=".">Read more...</a>
-                </div>
-                <a class="main_post_pic" href=".">
-                    <img alt="post preview" src="https://picsum.photos/200?random=${i}"/>
-                </a>
-            </section>`
-        )}
+    for(; i < 10; i++){
+        addPost(i)
     }
+    
+        
+});
 
-);
-
-function base64Encode(str) {
-    var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    var out = "", i = 0, len = str.length, c1, c2, c3;
-    while (i < len) {
-        c1 = str.charCodeAt(i++) & 0xff;
-        if (i == len) {
-            out += CHARS.charAt(c1 >> 2);
-            out += CHARS.charAt((c1 & 0x3) << 4);
-            out += "==";
-            break;
-        }
-        c2 = str.charCodeAt(i++);
-        if (i == len) {
-            out += CHARS.charAt(c1 >> 2);
-            out += CHARS.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-            out += CHARS.charAt((c2 & 0xF) << 2);
-            out += "=";
-            break;
-        }
-        c3 = str.charCodeAt(i++);
-        out += CHARS.charAt(c1 >> 2);
-        out += CHARS.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
-        out += CHARS.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
-        out += CHARS.charAt(c3 & 0x3F);
+$("#main_loadmore").click(function() {
+    let n = i
+    for(; i < n + 10; i++){
+            addPost(i)
     }
-    return out;
+})
+
+function addPost(id){
+    $(".main_container").append(`
+        <section class="main_post">
+            <div class="main_post_data">
+                <h2 class="main_post_title">Post ${id}</h2>
+                <p class="main_post_desc">${loremimpsum}</p>
+                <a class="main_post_link" href=".">Read more...</a>
+            </div>
+            <a class="main_post_pic" href="#">
+                <img alt="post preview" src="https://picsum.photos/200?random=${id}"/>
+            </a>
+        </section>`
+    )
 }
